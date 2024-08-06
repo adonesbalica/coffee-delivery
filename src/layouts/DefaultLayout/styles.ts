672cbfa090476d709fa2f982d15afa9db5852b81
Adones Balica <adonesbalica@gmail.com>
2024-08-06 15:13:35 -0300

    feat: Add base colors

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
-export const LayoutContainer = styled.div``
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 2.5rem;
+
+  display: flex;
+  flex-direction: column;
+`
